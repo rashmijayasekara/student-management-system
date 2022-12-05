95b81ce2c89218e5f328cff15b101c24ebfcedc0
Rashmi <rashmijayasekara00@gmail.com>
2022-12-05 19:27:48 +0530

    Create Add New Student UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddStudentComponent} from "./view/add-student/add-student.component";
+import {ViewStudentComponent} from "./view/view-student/view-student.component";
+import {WelcomeComponent} from "./view/welcome/welcome.component";
+import {NotFoundComponent} from "./view/not-found/not-found.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',
+    component:WelcomeComponent
+  },
+  {
+    path:'add',
+    component: AddStudentComponent
+
+  },
+  {
+    path:'search',
+    component:ViewStudentComponent
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddStudentComponent } from './view/add-student/add-student.component';
+import { ViewStudentComponent } from './view/view-student/view-student.component';
+import {MatButtonModule} from "@angular/material/button";
+import { WelcomeComponent } from './view/welcome/welcome.component';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    ViewStudentComponent,
+    WelcomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
